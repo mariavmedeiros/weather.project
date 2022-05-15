de45932062735aadec2625f1307b19b70e4d1be4
Maria Medeiros <maitevelori@hotmail.com>
2022-05-15 14:59:57 -0700

    added function temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round[response.data.wind.speed];
+  dateElement.innerHTML = formatDate[response.date.dt * 1000];
+}
+
 let apiKey = "30e80a3062954953f62b5ad133ae6616";
+let city = "Corralitos";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
